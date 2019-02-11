0faa4926663e0916fcfa47b38907f3670bdd0515
Mahmoud <mahmut.faris1993@gmail.com>
2019-02-11 16:46:03 +0100

    iofdhgoh

--- I-Project website/update.php
+++
@@ -1,9 +0,0 @@
-<?php
-include'recources/php/head.php';
-require'recources/php/functions/functie.php';
-
-if(isset($_GET['rubriek'])){
-  $desired = array($_GET['rubriek']); 
-  updateWhere(rubriek, $desired, $_GET['rubrieknummer'])
-
-?>
